c956033c495a492623a8243fc80f05f93ec30382
Yu Ge <yuge.me@gmail.com>
2018-03-29 12:18:22 +0800

    add dev prod environment

--- src/main/java/me/yuge/springwebflux/core/controller/UserController.java
+++ src/main/java/me/yuge/springwebflux/core/controller/UserController.java
@@ -3,10 +3,10 @@ package me.yuge.springwebflux.core.controller;
 import me.yuge.springwebflux.core.model.User;
 import me.yuge.springwebflux.core.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -19,13 +19,14 @@ public class UserController {
         this.userRepository = userRepository;
     }
 
-    @GetMapping("user")
-    public Mono<User> current(@AuthenticationPrincipal Mono<User> principal) {
-        return principal;
+    @GetMapping("users")
+    public Flux<User> all() {
+        return userRepository.findAll();
     }
 
-    @GetMapping("user/{username}")
-    public Mono<User> get(@PathVariable() String username) {
-        return this.userRepository.findByUsername(username);
+    @GetMapping("users/{id}")
+    public Mono<User> get(@PathVariable() String id) {
+        return userRepository.findById(id);
     }
+
 }
